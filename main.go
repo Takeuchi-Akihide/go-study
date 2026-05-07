74a3c985bdfa8b3538ca832305fac001fa22387c
Takeuchi-Akihide <htasn1110@gmail.com>
2026-05-07 20:25:17 +0900

    feat: day3 dbを追加

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type HealthResponse struct {
@@ -21,157 +22,80 @@ type User struct {
 	Name string `json:"name"`
 }
 
-type CreateUserRequest struct {
-	Name string `json:"name"`
+type App struct {
+	DB *sql.DB
 }
-
-type UpdateUserRequest struct {
-	Name string `json:"name"`
-}
-
-var users = map[int]User{
-	1: {ID: 1, Name: "Alice"},
-	2: {ID: 2, Name: "Bob"},
-}
-
-var nextID = 3
 
 func main() {
-	http.HandleFunc("/health", healthHandler)
-	http.HandleFunc("/users", usersHandler)
-	http.HandleFunc("/users/", userHandler)
+	dsn := "postgres://dev:password@localhost:5436/app_db?sslmode=disable"
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	app := &App{
+		DB: db,
+	}
+
+	http.HandleFunc("/health", app.healthHandler)
+	http.HandleFunc("/users", app.usersHandler)
 
 	log.Println("server started at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
+func (app *App) healthHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]string{"status": "ok"})
 }
 
-func usersHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		listUsers(w, r)
-	case http.MethodPost:
-		createUser(w, r)
-	default:
-		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{"error": "method not alowed"})
-	}
-}
-
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	id, ok := parseUserID(r.URL.Path)
-	if !ok {
-		writeJSON(w, http.StatusBadRequest, map[string]string{"error": "invalid user id"})
-		return
-	}
-
-	switch r.Method {
-	case http.MethodGet:
-		getUser(w, r, id)
-	case http.MethodPut:
-		updateUser(w, r, id)
-	case http.MethodDelete:
-		deleteUser(w, r, id)
-	default:
+func (app *App) usersHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
 		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{"error": "method not allowed"})
-	}
-}
-
-func listUsers(w http.ResponseWriter, r *http.Request) {
-	result := make([]User, 0, len(users))
-
-	for _, user := range users {
-		result = append(result, user)
-	}
-
-	writeJSON(w, http.StatusOK, result)
-}
-
-func getUser(w http.ResponseWriter, r *http.Request, id int) {
-	user, ok := users[id]
-	if !ok {
-		writeJSON(w, http.StatusNotFound, map[string]string{"error": "user not found"})
 		return
 	}
 
-	writeJSON(w, http.StatusOK, user)
-}
-
-func createUser(w http.ResponseWriter, r *http.Request) {
-	var req CreateUserRequest
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		writeJSON(w, http.StatusBadRequest, map[string]string{"error": "invalid json"})
-		return
-	}
-
-	if strings.TrimSpace(req.Name) == "" {
-		writeJSON(w, http.StatusBadRequest, map[string]string{"error": "name is required"})
-		return
-	}
-
-	user := User{
-		ID:   nextID,
-		Name: req.Name,
-	}
-
-	users[user.ID] = user
-	nextID++
-
-	writeJSON(w, http.StatusCreated, user)
-}
-
-func updateUser(w http.ResponseWriter, r *http.Request, id int) {
-	var req UpdateUserRequest
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		writeJSON(w, http.StatusBadRequest, map[string]string{"error": "invalid json"})
-		return
-	}
-
-	if strings.TrimSpace(req.Name) == "" {
-		writeJSON(w, http.StatusBadRequest, map[string]string{"error": "name is required"})
-		return
-	}
-
-	_, ok := users[id]
-	if !ok {
-		writeJSON(w, http.StatusNotFound, map[string]string{"error": "user not found"})
-		return
-	}
-
-	user := User{
-		ID:   id,
-		Name: req.Name,
-	}
-
-	users[id] = user
-
-	writeJSON(w, http.StatusOK, user)
-}
-
-func deleteUser(w http.ResponseWriter, r *http.Request, id int) {
-	_, ok := users[id]
-	if !ok {
-		writeJSON(w, http.StatusNotFound, map[string]string{"error": "user not found"})
-		return
-	}
-
-	delete(users, id)
-
-	writeJSON(w, http.StatusOK, map[string]string{"status": "deleted"})
-}
-
-func parseUserID(path string) (int, bool) {
-	idText := strings.TrimPrefix(path, "/users/")
-	id, err := strconv.Atoi(idText)
+	users, err := app.findUsers()
 	if err != nil {
-		return 0, false
+		log.Println("failed to find users:", err)
+		writeJSON(w, http.StatusInternalServerError, map[string]string{"error": "internal server error"})
+		return
+	}
+	writeJSON(w, http.StatusOK, users)
+}
+
+func (app *App) findUsers() ([]User, error) {
+	rows, err := app.DB.Query(`
+    SELECT id, name
+    FROM users
+    ORDER BY id
+  `)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users := []User{}
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Name)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
 	}
 
-	return id, true
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
 
 func writeJSON(w http.ResponseWriter, statusCode int, data any) {
